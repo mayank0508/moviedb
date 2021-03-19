272f404cc34a0e5d7e6857478a32a3025069988c
mayank0508 <thakurmayank0508@gmail.com>
2021-03-20 03:13:58 +0530

    document.id

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+const addMovieModal = document.getElementById('add-modal');
+// const modalaction = document.getElementsByClassName('modal__action');
+// const modalcontent = document.getElementsByClassName('modal__content');
+
+console.log(addMovieModal);
