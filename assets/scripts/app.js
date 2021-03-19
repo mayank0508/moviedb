e4fd16462257181bd70933e4611ab2f40e36eec6
mayank0508 <thakurmayank0508@gmail.com>
2021-03-20 03:22:46 +0530

    document.without.id

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,9 @@
 const addMovieModal = document.getElementById('add-modal');
+
+const startAddMovieModal = document.querySelector('header button')
+//const startAddMovieModal = document.querySelector('header').lastElementChild;
 // const modalaction = document.getElementsByClassName('modal__action');
 // const modalcontent = document.getElementsByClassName('modal__content');
 
 console.log(addMovieModal);
+console.log(startAddMovieModal);
