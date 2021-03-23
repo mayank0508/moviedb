3a61959ac3df9a682b3173bd0ce5b3c39b2226f9
mayank0508 <thakurmayank0508@gmail.com>
2021-03-24 03:00:55 +0530

    backdrop added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,19 @@
 const addMovieModal = document.getElementById('add-modal');
-
+const backgroundModel = document.getElementById('backdrop');
 const startAddMovieModal = document.querySelector('header button')
 //const startAddMovieModal = document.querySelector('header').lastElementChild;
 // const modalaction = document.getElementsByClassName('modal__action');
 // const modalcontent = document.getElementsByClassName('modal__content');
+// const backgroundModal = document.body.firstElementChild;
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdropModal();
+}
+
+const toggleBackdropModal = () => {
+   backgroundModel.classList.toggle('visible');
 }
 
 
-startAddMovieModal.addEventListener('click',toggleMovieModal);
+startAddMovieModal.addEventListener('click',toggleMovieModal,toggleBackdropModal);
