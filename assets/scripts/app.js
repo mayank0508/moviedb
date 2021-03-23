c1651d516fa8c34c3d4b3bc95378ed6bdd237948
mayank0508 <thakurmayank0508@gmail.com>
2021-03-24 03:29:01 +0530

    enabled cancel and backdrop featue to shut down the modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const addMovieModal = document.getElementById('add-modal');
 const backgroundModel = document.getElementById('backdrop');
-const startAddMovieModal = document.querySelector('header button')
+const startAddMovieModal = document.querySelector('header button');
+const cancelModal = addMovieModal.querySelector('.btn--passive');
 //const startAddMovieModal = document.querySelector('header').lastElementChild;
 // const modalaction = document.getElementsByClassName('modal__action');
 // const modalcontent = document.getElementsByClassName('modal__content');
@@ -15,5 +16,15 @@ const toggleBackdropModal = () => {
    backgroundModel.classList.toggle('visible');
 }
 
+const backdropClick = () => {
+    toggleMovieModal();
+}
 
-startAddMovieModal.addEventListener('click',toggleMovieModal,toggleBackdropModal);
+const CancelButton = () => {
+   toggleMovieModal();
+}
+
+
+startAddMovieModal.addEventListener('click',toggleMovieModal);
+backgroundModel.addEventListener('click',toggleMovieModal);
+cancelModal.addEventListener('click',toggleMovieModal);
