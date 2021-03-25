fd74ce552086b87e6c22b60dc1a5176f6711a596
mayank0508 <thakurmayank0508@gmail.com>
2021-03-26 03:04:52 +0530

    add button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,9 @@ const addMovieModal = document.getElementById('add-modal');
 const backgroundModel = document.getElementById('backdrop');
 const startAddMovieModal = document.querySelector('header button');
 const cancelModal = addMovieModal.querySelector('.btn--passive');
+const addMovieButton = cancelModal.nextElementSibling;
+const inputModal = addMovieButton.querySelectorAll('input');
+//const inputModal = document.getElementsByTagName('input');
 //const startAddMovieModal = document.querySelector('header').lastElementChild;
 // const modalaction = document.getElementsByClassName('modal__action');
 // const modalcontent = document.getElementsByClassName('modal__content');
@@ -24,7 +27,24 @@ const CancelButton = () => {
    toggleMovieModal();
 }
 
+const addButton = () => {
+   const titleValue = userInput[0].value;
+   const imageURLValue = userInput[1].value;
+   const ratingValue = userInput[2].value;
+
+   if (
+    titleValue === " " ||
+    imageURLValue === " " ||
+    ratingValue === " " ||
+    +ratingValue < 1 || 
+    +rating > 5
+    ) {
+        alert("Plz enter valid inputs !")
+        return;
+    }
+}
 
 startAddMovieModal.addEventListener('click',toggleMovieModal);
 backgroundModel.addEventListener('click',backdropClick);
 cancelModal.addEventListener('click',CancelButton);
+addMovieButton.addEventListener('click',addButton)
